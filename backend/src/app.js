2795b7089c3f0dddb8bcc38d4e713e33c723a094
Aashritha Sandiri <aashrithas2000@gmail.com>
2024-02-24 15:20:03 -0600

    Added multiplication router for practice

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,16 @@ app.get('/add', (req, res) => {
   return res.json({ message: "addition of two number ", result: c })
 
 })
+
+
+app.get('/mul',(req,res)=>{
+  console.log(req.query)
+  const {a, b} = req.query
+  console.log('Entered into Multiplication block')
+  const z = Number(a)*Number(b)
+  console.log(z)
+  return res.send(`Multiplication of 2 numbers is ${z}`)
+})
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
 })
