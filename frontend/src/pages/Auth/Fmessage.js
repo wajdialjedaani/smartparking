aef190eccb3f96f7a0ec140ecc2684765f884272
Bhavani Adepu <bhavaniadepu@Bhavanis-Air.lan>
2024-03-17 16:57:16 -0500

    Identified bug for submit button in Admin page in create page

--- frontend/src/pages/Auth/Fmessage.js
+++ frontend/src/pages/Auth/Fmessage.js
@@ -2,8 +2,8 @@ import React from "react";
 function Fmessage() {
   return (
     <div class="flex justify-center items-center">
-    <div class="text-3xl bg-black text-slate-50 h-[200px] w-[400px] flex justify-center items-center my-10 ">
-     <h1 class=""> Thanks for your feedback</h1>
+    <div class="text-3xl bg-black text-slate-50 rounded-xl h-[200px] w-[400px] flex justify-center items-center my-10 ">
+     <h1 class=""> Thank you for your feedback</h1>
     </div>
     </div>
     
